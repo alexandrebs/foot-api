228e1f439e555986db371fb8ef8f924cfa1bd3a8
alexandre <alexandrebs10@gmail.com>
2022-03-25 19:59:04 -0300

    first commit

---
+++ src/main/java/com/brazucabet/brazucaapi/BrazucaApiApplication.java
@@ -0,0 +1,13 @@
+package com.brazucabet.brazucaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazucaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazucaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brazucabet/brazucaapi/BrazucaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazucabet.brazucaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazucaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
