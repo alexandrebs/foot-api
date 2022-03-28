2596e19b4fd56664e5471f2480fd3bf943e03e04
alexandre <alexandrebs10@gmail.com>
2022-03-28 08:33:41 -0300

    Recupenado gols do jogador

--- src/main/java/com/brazucabet/brazucaapi/util/ScrapingUtil.java
+++ src/main/java/com/brazucabet/brazucaapi/util/ScrapingUtil.java
@@ -5,15 +5,18 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.lang.model.element.Element;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.server.Encoding;
@@ -62,9 +65,12 @@ public class ScrapingUtil {
 
 				String placarCasa = obtemPlacarCasa(document);
 				LOGGER.info("Placar casa: {}", placarCasa);
-				
+
 				String placarVisitante = obtemPlacarVisitante(document);
 				LOGGER.info("Placar visitante: {}", placarVisitante);
+
+				String golsJogadorCasa = obtemGolsJogadosCasa(document);
+				LOGGER.info("Gols Jogador Casa: {}", golsJogadorCasa);
 			}
 
 			String nomeEquipeCasa = obtemNomeEquipeCasa(document);
@@ -219,10 +225,40 @@ public class ScrapingUtil {
 
 		return placarVisitante;
 	}
+
 	
+	private String obtemGolsJogadosCasa(Document document) {
+
+		ArrayList<String> golsCasa  = new ArrayList<>();
+		
+		Elements elements = document.selectFirst("div[class=imso_gs__tgs imso_gs__left-team]").select("div[class=imso_gs__gs-r]");
+		
+		for (org.jsoup.nodes.Element e : elements) {
+		
+			String golsAux = e.select("div[class=imso_gs__gs-r]").text();
+			golsCasa.add(golsAux);
+		}
+		
+		return String.join(", ", golsCasa).replace("'", " min");
+	}
+
+	private String obtemGolsJogadosVisitante(Document document) {
+
+		ArrayList<String> golsVisitante  = new ArrayList<>();
+		
+		Elements elements = document.selectFirst("div[class=imso_gs__tgs imso_gs__right-team]").select("div[class=imso_gs__gs-r]");
+		
+		for (org.jsoup.nodes.Element e : elements) {
+		
+			String golsAux = e.select("div[class=imso_gs__gs-r]").text();
+			golsVisitante.add(golsAux);
+		}
+		
+		return String.join(", ", golsVisitante).replace("'", " min");
+	}
 	
 	private void request() throws IOException {
-		
+
 		URL url = null;
 		try {
 			url = new URL("https://www.uol.com.br/esporte/service/?loadComponent=match-service&contentType=json");
@@ -234,25 +270,27 @@ public class ScrapingUtil {
 		httpConn.setRequestMethod("GET");
 
 		httpConn.setRequestProperty("authority", "www.uol.com.br");
-		httpConn.setRequestProperty("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"");
+		httpConn.setRequestProperty("sec-ch-ua",
+				"\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"");
 		httpConn.setRequestProperty("accept", "application/json, text/javascript, */*; q=0.01");
 		httpConn.setRequestProperty("x-requested-with", "XMLHttpRequest");
 		httpConn.setRequestProperty("sec-ch-ua-mobile", "?0");
-		httpConn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36");
+		httpConn.setRequestProperty("user-agent",
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36");
 		httpConn.setRequestProperty("sec-ch-ua-platform", "\"Windows\"");
 		httpConn.setRequestProperty("sec-fetch-site", "same-origin");
 		httpConn.setRequestProperty("sec-fetch-mode", "cors");
 		httpConn.setRequestProperty("sec-fetch-dest", "empty");
 		httpConn.setRequestProperty("referer", "https://www.uol.com.br/esporte/futebol/central-de-jogos/");
 		httpConn.setRequestProperty("accept-language", "pt-BR,pt;q=0.9,en;q=0.8,de;q=0.7");
-		httpConn.setRequestProperty("cookie", "BTCTL=a3; _hjTLDTest=1; TS011ad88a=0160ed0a6cf848fc4717dd78d6b39390384ddec07c1c9d74fcc86bafb922d194ee23c6355ed426c37a00c6cc9bca7475aa7736151ab0c82efeb5781113bc63f4cdb4daa1bf26e1798266945edddb2f34432525b1f8eb75c31f319a87fcd791548ef5e89ee971d9e46a58558cd3d49bf3913e6a1cce99da14764d3531f823204e493d7a0eb3; nvg18889=b1d369269d668d1bc4e73f9e609|2_91; nvg72696=b1d3692694eaefa02ba13df6b09|0_110; _matheriSegs=MATHER_U9_INSTANTMET2_20200701; _matherSegments=MATHER_U9_INSTANTMET2_20200701; DNA=a37a74b18b49466da3885016bece82c0|179e7896f96|true; _cb_ls=1; _cb=YE_wCycbhXz9AKD; cX_S=kq11p4gwbblt3ggd; cX_G=cx%3A9juzrxhdkcck1a5q2kexafx10%3Aqzz7o6kla9tw; tt.u=0100007F51CE7660AD068F4D02038F27; _sp_id.7839=3f569953ac6ff9f8.1621262289.4.1624617782.1622633003; _cc_id=2d20ebd05894649d44b14df6746bc0bd; _cb=DdRc05Bn5iiaBjpojp; _fbp=fb.2.1643630789850.1486112385; __tbc=%7Bkpex%7DFEjHp3gg5mhcRc5YvkWDjCK81kSZEljFNkTkwIl-Kq5ii14KxuceDQ6g1cTJxQqX; cX_P=kq11p3c9hc25qvaf; _ga_V46CYWTD2Z=GS1.1.1643640408.2.0.1643640408.60; UOLAF.CAD=0000017f-c36a-2aa4-0000-01805de8f2a4; UOL_FS=clicklogger.rm.uol.com.br; _ugfc=1; _pbjs_userid_consent_data=3524755945110770; __gads=ID=5273218535db7353:T=1648251002:S=ALNI_MbDNG9UwtHyxhMzG6yGwIFZh7uXnA; __pat=10800000; xbc=%7Bkpex%7DxPnhvOwi6uC6PGS1SQCob2q-edjJy4z9dPYR7Fdh_l7Mc9W_nyN8lCPcu8yk9OQeUHsvkNZj8lEOV80oBQBsBkMNWKcTr8mpoGptyncc03sMiEbH9-aTlD6_S25FVw13r-Nhzl-3mLwHzjudqh3inS83cs2ZP3PtfseLNZZFoTBzlhgEW6xNKjDV8qKm-9Y_ZUQrPmzIo2KbBw8VqRYgvvQc9oWRDj3nQ1qCnBOuZ9UNi9HhCGcxm2b-oBH-Ul_JWZtmGqDqeoDY70if0owi1Q; _gid=GA1.3.735297500.1648381561; tt.nprf=; nav23947=b1d369269fd075aa4d3d30a2b09|2_87; _ga_ZR3VDENZ5K=GS1.1.1648406289.3.0.1648406289.60; _ga=GA1.3.148775184.1611706525; _chartbeat2=.1623942935787.1648423672173.0000000000000101.f5XjUCffG5CC6LCPuBvhkrotZq8o.1; _cb_svref=https%3A%2F%2Fwww.uol.com.br%2Fesporte%2Ffutebol%2Fcampeonatos%2F; __pvi=%7B%22id%22%3A%22v-l19wwbule93mdgvc%22%2C%22domain%22%3A%22.uol.com.br%22%2C%22time%22%3A1648423679997%7D; FCNEC=[[\"AKsRol-oSQeQYd1yIAiWZ1fVAHqeWaZPbuUlb24ChhChgY6qNzaX8y1olEhYv-vnq1hTtZCD-f8NxRQFSNdqotjwDQ5laTHoH8zKbGQYatLQ50untBoYOOmcnsK0evHrsTEKp2L-cyr-xAXSTZg-xQKWGrhBukgCyQ==\"],null,[]]; tt_c_vmt=1648423686; tt_c_c=direct; tt_c_s=direct; tt_c_m=direct; _ttuu.s=1648423687362; _hjIncludedInSessionSample=0");
+		httpConn.setRequestProperty("cookie",
+				"BTCTL=a3; _hjTLDTest=1; TS011ad88a=0160ed0a6cf848fc4717dd78d6b39390384ddec07c1c9d74fcc86bafb922d194ee23c6355ed426c37a00c6cc9bca7475aa7736151ab0c82efeb5781113bc63f4cdb4daa1bf26e1798266945edddb2f34432525b1f8eb75c31f319a87fcd791548ef5e89ee971d9e46a58558cd3d49bf3913e6a1cce99da14764d3531f823204e493d7a0eb3; nvg18889=b1d369269d668d1bc4e73f9e609|2_91; nvg72696=b1d3692694eaefa02ba13df6b09|0_110; _matheriSegs=MATHER_U9_INSTANTMET2_20200701; _matherSegments=MATHER_U9_INSTANTMET2_20200701; DNA=a37a74b18b49466da3885016bece82c0|179e7896f96|true; _cb_ls=1; _cb=YE_wCycbhXz9AKD; cX_S=kq11p4gwbblt3ggd; cX_G=cx%3A9juzrxhdkcck1a5q2kexafx10%3Aqzz7o6kla9tw; tt.u=0100007F51CE7660AD068F4D02038F27; _sp_id.7839=3f569953ac6ff9f8.1621262289.4.1624617782.1622633003; _cc_id=2d20ebd05894649d44b14df6746bc0bd; _cb=DdRc05Bn5iiaBjpojp; _fbp=fb.2.1643630789850.1486112385; __tbc=%7Bkpex%7DFEjHp3gg5mhcRc5YvkWDjCK81kSZEljFNkTkwIl-Kq5ii14KxuceDQ6g1cTJxQqX; cX_P=kq11p3c9hc25qvaf; _ga_V46CYWTD2Z=GS1.1.1643640408.2.0.1643640408.60; UOLAF.CAD=0000017f-c36a-2aa4-0000-01805de8f2a4; UOL_FS=clicklogger.rm.uol.com.br; _ugfc=1; _pbjs_userid_consent_data=3524755945110770; __gads=ID=5273218535db7353:T=1648251002:S=ALNI_MbDNG9UwtHyxhMzG6yGwIFZh7uXnA; __pat=10800000; xbc=%7Bkpex%7DxPnhvOwi6uC6PGS1SQCob2q-edjJy4z9dPYR7Fdh_l7Mc9W_nyN8lCPcu8yk9OQeUHsvkNZj8lEOV80oBQBsBkMNWKcTr8mpoGptyncc03sMiEbH9-aTlD6_S25FVw13r-Nhzl-3mLwHzjudqh3inS83cs2ZP3PtfseLNZZFoTBzlhgEW6xNKjDV8qKm-9Y_ZUQrPmzIo2KbBw8VqRYgvvQc9oWRDj3nQ1qCnBOuZ9UNi9HhCGcxm2b-oBH-Ul_JWZtmGqDqeoDY70if0owi1Q; _gid=GA1.3.735297500.1648381561; tt.nprf=; nav23947=b1d369269fd075aa4d3d30a2b09|2_87; _ga_ZR3VDENZ5K=GS1.1.1648406289.3.0.1648406289.60; _ga=GA1.3.148775184.1611706525; _chartbeat2=.1623942935787.1648423672173.0000000000000101.f5XjUCffG5CC6LCPuBvhkrotZq8o.1; _cb_svref=https%3A%2F%2Fwww.uol.com.br%2Fesporte%2Ffutebol%2Fcampeonatos%2F; __pvi=%7B%22id%22%3A%22v-l19wwbule93mdgvc%22%2C%22domain%22%3A%22.uol.com.br%22%2C%22time%22%3A1648423679997%7D; FCNEC=[[\"AKsRol-oSQeQYd1yIAiWZ1fVAHqeWaZPbuUlb24ChhChgY6qNzaX8y1olEhYv-vnq1hTtZCD-f8NxRQFSNdqotjwDQ5laTHoH8zKbGQYatLQ50untBoYOOmcnsK0evHrsTEKp2L-cyr-xAXSTZg-xQKWGrhBukgCyQ==\"],null,[]]; tt_c_vmt=1648423686; tt_c_c=direct; tt_c_s=direct; tt_c_m=direct; _ttuu.s=1648423687362; _hjIncludedInSessionSample=0");
 		httpConn.setRequestProperty("if-none-match", "\"62434eb14f11cbd97b965fd93d3742f3\"");
 		httpConn.setRequestProperty("if-modified-since", "Sun, 27 Mar 2022 22:59:53 GMT");
 
 		InputStream responseStream = null;
 		try {
-			responseStream = httpConn.getResponseCode() / 100 == 2
-					? httpConn.getInputStream()
+			responseStream = httpConn.getResponseCode() / 100 == 2 ? httpConn.getInputStream()
 					: httpConn.getErrorStream();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
